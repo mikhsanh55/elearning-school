09c6f6d0c766facf7a10fadb3ac5138c3ea74ad5
mikhsanh <ikhsanhamidan13@gmail.com>
2020-06-30 18:52:52 +0700

    Ashiap

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ header("Expires: Thu, 19 Nov 1981 08:52:00 GMT");
 header("Cache-Control: no-store, no-cache, must-revalidate");
 
 /**
- * CodeIgniter Hai
+ * CodeIgniter Haid
  *
  * An open source application development framework for PHP
  *
