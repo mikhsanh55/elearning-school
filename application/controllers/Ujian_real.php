b07eadc275a57ca7a320246b9a09b4e2738737d4
mikhsanh <ikhsanhamidan13@gmail.com>
2020-07-09 08:06:31 +0700

    test

--- application/controllers/Ujian_real.php
+++ application/controllers/Ujian_real.php
@@ -109,8 +109,10 @@ class Ujian_real extends MY_Controller {
 			$mapel = $this->m_mapel->get_many_by(['id_instansi' => $this->akun->instansi]);
 		}
 		else if($this->log_lvl == 'guru') {
-			$kelas = $this->m_kelas->get_data_mapel(['kls.id_instansi' => $this->akun->instansi, 'dkmapel.id_guru' => $this->akun->id]);
-			$mapel = $this->m_kelas->get_data_mapel(['mapel.id_instansi' => $this->akun->instansi, 'dkmapel.id_guru' => $this->akun->id]);
+			// $kelas = $this->m_kelas->get_data_mapel(['kls.id_instansi' => $this->akun->instansi, 'dkmapel.id_guru' => $this->akun->id]);
+			$kelas = $this->m_kelas->get_all(['kls.id_instansi' => $this->akun->instansi]);
+			// $mapel = $this->m_kelas->get_data_mapel(['mapel.id_instansi' => $this->akun->instansi, 'dkmapel.id_guru' => $this->akun->id]);
+			$mapel = $this->m_mapel->get_many_by(['id_instansi' => $this->akun->instansi]);
 
 		}
 		
