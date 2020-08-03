51a7d544ea23f05ff9785383fb3ce72e8ef0ff0e
mikhsanh <ikhsanhamidan13@gmail.com>
2020-08-03 13:19:49 +0700

    tampilkan gambar soal di list soal ujian

--- application/views/ujian/table_soal.php
+++ application/views/ujian/table_soal.php
@@ -4,6 +4,7 @@
 		<tr>
 			<th class="frist"><input type="checkbox" name="checkall" id="checkall"></th>
 			<th class="frist">No</th>
+			<th>Gambar</th>
 			<th>Soal</th>
 			<!-- <th>Analisa</th> -->
 		
@@ -11,30 +12,76 @@
 		<?php 
 			if (count($paginate['data']) > 0) {
 			$i= $page_start; foreach ($paginate['data'] as $rows):
-			$opsi_a = preg_replace("/[#]/", "", $rows->opsi_a);
-			$opsi_b = preg_replace("/[#]/", "", $rows->opsi_b);
-			$opsi_c = preg_replace("/[#]/", "", $rows->opsi_c);
-			$opsi_d = preg_replace("/[#]/", "", $rows->opsi_d);
-			$opsi_e = preg_replace("/[#]/", "", $rows->opsi_e);
+			$opsi_a = explode('#####', $rows->opsi_a);
+			$gambar_a = !empty($opsi_a[0]) ? $opsi_a[0] : NULL;
+
+			$opsi_b = explode('#####', $rows->opsi_b);
+			$gambar_b = !empty($opsi_b[0]) ? $opsi_b[0] : NULL;
+
+			$opsi_c = explode('#####', $rows->opsi_c);
+			$gambar_c = !empty($opsi_c[0]) ? $opsi_c[0] : NULL;
+
+			$opsi_d = explode('#####', $rows->opsi_d);
+			$gambar_d = !empty($opsi_d[0]) ? $opsi_d[0] : NULL;
+
+			$opsi_e = explode('#####', $rows->opsi_e);
+			$gambar_e = !empty($opsi_e[0]) ? $opsi_e[0] : NULL;
 
 		?>
 			<tr>
 				<td><input type="checkbox" name="checklist[]" class="checklist" data-id= "<?=$rows->id;?>" value="<?=$rows->id;?>"></td>
 				<td align="center" class="frist"><?=$i;?></td>
+				<td>
+					<?php if(!empty($rows->file)) : ?>
+						<img src="<?= base_url('upload/file_ujian_soal/') . $rows->file; ?>" alt="" class="img-thumbnail" width="80" height="80">
+					<?php endif; ?>
+				</td>
 				<td><?=$rows->soal;?></td>
 			</tr>
+				
 			<tr>
-				<td colspan="2"><button class="btn btn-sm btn-block btn-primary btn-opsi" data-toggle="collapse" data-target="#opsi<?= $i; ?>" aria-expanded="false" aria-controls="opsi<?= $i; ?>"><i class="fas fa-angle-double-right" ></i></button></td>
+				<td colspan="3"><button class="btn btn-sm btn-block btn-primary btn-opsi" data-toggle="collapse" data-target="#opsi<?= $i; ?>" aria-expanded="false" aria-controls="opsi<?= $i; ?>"><i class="fas fa-angle-double-right" ></i></button></td>
 				<td>
 					<p>Tekan tombol <i class="fas fa-angle-double-right"></i> untuk melihat opsi</p>
 					<div class="collapse" id="opsi<?= $i; ?>">
 						<div class="card card-body">
 							<ul>
-								<li class="d-flex flex-row"><span>A. </span> <?= $opsi_a; ?></li>
-								<li class="d-flex flex-row"><span>B. </span> <?= $opsi_b; ?></li>
-								<li class="d-flex flex-row"><span>C. </span> <?= $opsi_c; ?></li>
-								<li class="d-flex flex-row"><span>D. </span> <?= $opsi_d; ?></li>
-								<li class="d-flex flex-row"><span>E. </span> <?= $opsi_e; ?></li>
+								<li class="d-flex flex-row ">
+									<div>A. </div><?= end($opsi_a); ?>
+										<?php if(!is_null($gambar_a)) { ?> 
+											<img src="<?= base_url('upload/file_ujian_opsi/') . $gambar_a;?>" alt="" width="100" height="100">
+										<?php } ?> 
+									 
+								</li>
+								<li class="d-flex flex-row ">
+									<div>B. </div><?= end($opsi_b); ?>
+										<?php if(!is_null($gambar_b)) { ?> 
+											<img src="<?= base_url('upload/file_ujian_opsi/') . $gambar_b;?>" alt="" width="100" height="100">
+										<?php } ?> 
+									 
+								</li>
+								
+								<li class="d-flex flex-row ">
+									<div>C. </div><?= end($opsi_c); ?>
+										<?php if(!is_null($gambar_c)) { ?> 
+											<img src="<?= base_url('upload/file_ujian_opsi/') . $gambar_c;?>" alt="" width="100" height="100">
+										<?php } ?> 
+									 
+								</li>
+								<li class="d-flex flex-row ">
+									<div>D. </div><?= end($opsi_d); ?>
+										<?php if(!is_null($gambar_d)) { ?> 
+											<img src="<?= base_url('upload/file_ujian_opsi/') . $gambar_d;?>" alt="" width="100" height="100">
+										<?php } ?> 
+									 
+								</li>
+								<li class="d-flex flex-row ">
+									<div>E. </div><?= end($opsi_e); ?>
+										<?php if(!is_null($gambar_e)) { ?> 
+											<img src="<?= base_url('upload/file_ujian_opsi/') . $gambar_e;?>" alt="" width="100" height="100">
+										<?php } ?> 
+									 
+								</li>
 							</ul>
 						</div>
 					</div>
