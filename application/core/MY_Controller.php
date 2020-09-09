17139e819f0cfdb1e926e65289bb5c663549bfcc
mikhsanh <ikhsanhamidan13@gmail.com>
2020-09-09 12:44:37 +0700

    hotfix

--- application/core/MY_Controller.php
+++ application/core/MY_Controller.php
@@ -99,15 +99,15 @@ class MY_Controller extends CI_Controller
 		date_default_timezone_set('Asia/Jakarta');
 		$this->load->dbforge();
 		// Alter table
-		$fields = [
-			'status_ujian' => ['type' => 'TINYINT']
-		];
-		$this->dbforge->add_column('tb_ikut_ujian', $fields);
+		// $fields = [
+		// 	'status_ujian' => ['type' => 'TINYINT']
+		// ];
+		// $this->dbforge->add_column('tb_ikut_ujian', $fields);
 
-		$fields = [
-			'status_ujian' => ['type' => 'TINYINT']
-		];
-		$this->dbforge->add_column('tb_ikut_ujian_essay', $fields);
+		// $fields = [
+		// 	'status_ujian' => ['type' => 'TINYINT']
+		// ];
+		// $this->dbforge->add_column('tb_ikut_ujian_essay', $fields);
 
 
 
